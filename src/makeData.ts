a673b993d029e44afb3f7806f21136074e88737f
bharathserion2025 <bharath.kadiri@servion.com>
2024-12-06 16:22:27 +0530

    mock data changes

--- src/makeData.ts
+++ src/makeData.ts
@@ -9,60 +9,12 @@ export type User = {
 export const fakeData: User[] = [
   {
     id: '9s41rp',
-    firstName: 'Kelvin',
-    lastName: 'Langosh',
-    email: 'Jerod14@hotmail.com',
+    firstName: 'Item1',
+    lastName: '2$',
+    email: 'A',
     state: 'Ohio',
-  },
-  {
-    id: '08m6rx',
-    firstName: 'Molly',
-    lastName: 'Purdy',
-    email: 'Hugh.Dach79@hotmail.com',
-    state: 'Rhode Island',
-  },
-  {
-    id: '5ymtrc',
-    firstName: 'Henry',
-    lastName: 'Lynch',
-    email: 'Camden.Macejkovic@yahoo.com',
-    state: 'California',
-  },
-  {
-    id: 'ek5b97',
-    firstName: 'Glenda',
-    lastName: 'Douglas',
-    email: 'Eric0@yahoo.com',
-    state: 'Montana',
-  },
-  {
-    id: 'xxtydd',
-    firstName: 'Leone',
-    lastName: 'Williamson',
-    email: 'Ericka_Mueller52@yahoo.com',
-    state: 'Colorado',
-  },
-  {
-    id: 'wzxj9m',
-    firstName: 'Mckenna',
-    lastName: 'Friesen',
-    email: 'Veda_Feeney@yahoo.com',
-    state: 'New York',
-  },
-  {
-    id: '21dwtz',
-    firstName: 'Wyman',
-    lastName: 'Jast',
-    email: 'Melvin.Pacocha@yahoo.com',
-    state: 'Montana',
-  },
-  {
-    id: 'o8oe4k',
-    firstName: 'Janick',
-    lastName: 'Willms',
-    email: 'Delfina12@gmail.com',
-    state: 'Nebraska',
-  },
+  }
+  
 ];
 
 //50 us states array
